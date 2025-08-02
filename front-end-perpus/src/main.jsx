d8ed7ab1fc6aacbc29ef98f9a7b44ae356f4cfcb
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-08-02 21:51:31 +0700

    add routing for EditBooks, Members, AddMembers, EditMembers, and ErrorPage components

--- front-end-perpus/src/main.jsx
+++ front-end-perpus/src/main.jsx
@@ -6,6 +6,11 @@ import App from './App.jsx';
 import Dashboard from './pages/Dashboard/index.jsx';
 import Books from './pages/Books/index.jsx';
 import AddBooks from './pages/Books/AddBooks.jsx';
+import EditBooks from './pages/Books/EditBooks.jsx';
+import Members from './pages/Members/index.jsx';
+import AddMembers from './pages/Members/AddMembers.jsx';
+import EditMembers from './pages/Members/EditMembers.jsx';
+import ErrorPage from './pages/404.jsx';
 
 const router = createBrowserRouter([
 	{
@@ -23,6 +28,26 @@ const router = createBrowserRouter([
 	{
 		path: '/tambah-buku',
 		element: <AddBooks />,
+	},
+	{
+		path: '/edit-buku/:id',
+		element: <EditBooks />,
+	},
+	{
+		path: '/daftar-anggota',
+		element: <Members />,
+	},
+	{
+		path: '/tambah-anggota',
+		element: <AddMembers />,
+	},
+	{
+		path: '/edit-anggota/:id',
+		element: <EditMembers />,
+	},
+	{
+		path: '*',
+		element: <ErrorPage />,
 	},
 ]);
 
